ca8aba29b00ca8326e8820e016cfc09f3adec112
samyak <samyakmhrzn9841@gmail.com>
2024-12-01 22:23:18 +0545

    file structure dump

---
+++ src/app/components/authentication/Authentication.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./authentication.module.css"
+
+type Props = {}
+
+const Authentication = (props: Props) => {
+  return (
+    <div>Authentication Component</div>
+  )
+}
+
+export default Authentication
