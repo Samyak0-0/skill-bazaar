ec225a130d10fb05dfc6402e0582580d7a03b4e7
kreeteeshakya <shakya.kreetee@gmail.com>
2024-12-02 12:03:27 +0545

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Authentication from "./components/authentication/Authentication";
 import Home_page from "./components/home_page/Home_page";
 import Messaging from "./components/messaging/Messaging";
+import User_profile from "./components/user_profile/User_profile";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Authentication />
       <Messaging />
       <Home_page />
+      <User_profile/>
     </div>
   );
 }
