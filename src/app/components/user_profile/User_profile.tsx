89c5056a7ac6aa58b9ab2d39300c00672648685b
kreeteeshakya <shakya.kreetee@gmail.com>
2024-12-02 11:58:48 +0545

    created user profile

---
+++ src/app/components/user_profile/User_profile.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const User_profile = (props: Props) => {
+    return (
+        <div>
+           <h2> User profile</h2>
+        </div>
+    )
+}
+
+export default User_profile
