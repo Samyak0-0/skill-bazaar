f1f132eedb1617c4b4a5ea73d032cce619d1eb9b
samyak <samyakmhrzn9841@gmail.com>
2024-12-04 16:37:06 +0545

    pages for routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
-import Authentication from "./components/authentication/Authentication";
-import Home_page from "./components/home_page/Home_page";
-import Messaging from "./components/messaging/Messaging";
-import User_profile from "./components/user_profile/User_profile";
-import Navbar from "./components/navbar/Navbar";
+import Home_page from "../components/home_page/Home_page";
+import Navbar from "../components/navbar/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <div>
-        <Authentication />
-        <Messaging />
-        <Home_page />
-        <User_profile />
-      </div>
+      <Home_page />
     </div>
   );
 }
---
+++ src/components/order/Order.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Order = (props: Props) => {
+  return (
+    <div>Order Component</div>
+  )
+}
+
+export default Order
