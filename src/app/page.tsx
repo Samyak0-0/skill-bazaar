501dbeec4aa3163f2bf64c804ae0570ff1b6bdc6
Sagar Parajuli <parajuli.sagar759@gmail.com>
2024-12-04 14:53:31 +0545

    tried stuff with navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@
 import Authentication from "./components/authentication/Authentication";
 import Home_page from "./components/home_page/Home_page";
 import Messaging from "./components/messaging/Messaging";
+import Navbar from "./components/navbar/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Authentication />
-      <Messaging />
-      <Home_page />
+      <Navbar/>
+      <div>
+        <Authentication />
+        <Messaging />
+        <Home_page />
+    </div>
     </div>
   );
 }
