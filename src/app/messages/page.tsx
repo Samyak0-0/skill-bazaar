b81debc34af48c7622e3b1d7234e005c522d142e
samyak <samyakmhrzn9841@gmail.com>
2024-12-08 21:54:01 +0545

    contacts + chat space

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,12 +1,13 @@
 import Messaging from "@/components/messaging/Messaging";
 import React from "react";
-
+import ContactSection from "@/components/messaging/Contacts";
 type Props = {};
 
 const MessagingPage = (props: Props) => {
   return (
     <>
       <Messaging />
+      <ContactSection />
     </>
   );
 };
---
+++ src/components/messaging/Contacts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactSection = () => {
+  return (
+    <div>Contacts</div>
+  )
+}
+
+export default ContactSection
