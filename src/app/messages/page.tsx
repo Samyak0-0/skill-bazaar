3a04105a5c5dfe9eb05ba772a7e84de33df312f4
samyak <samyakmhrzn9841@gmail.com>
2025-01-01 16:50:35 +0545

    authentication-2

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Messaging from "@/components/messaging/Messaging";
 import React from "react";
@@ -8,13 +8,11 @@ import { useSession } from "next-auth/react";
 type Props = {};
 
 const MessagingPage = (props: Props) => {
+  const { data } = useSession();
 
-  const {data} = useSession();
-  
-  if(data?.user?.name) {
+  if (data?.user?.name) {
     return (
       <div className="bg-red-100 w-full h-[90vh] flex">
-        
         <ContactSection />
         <Messaging />
       </div>
