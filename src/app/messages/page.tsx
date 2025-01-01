3de47f39334682964f3b0d95cc07ea955959d406
samyak <samyakmhrzn9841@gmail.com>
2025-01-01 16:47:36 +0545

    authentication

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,13 +1,29 @@
+"use client"
+
 import Messaging from "@/components/messaging/Messaging";
 import React from "react";
 import ContactSection from "@/components/messaging/Contacts";
+import { useSession } from "next-auth/react";
+
 type Props = {};
 
 const MessagingPage = (props: Props) => {
+
+  const {data} = useSession();
+  
+  if(data?.user?.name) {
+    return (
+      <div className="bg-red-100 w-full h-[90vh] flex">
+        
+        <ContactSection />
+        <Messaging />
+      </div>
+    );
+  }
+
   return (
-    <div className="bg-red-100 w-full h-[90vh] flex">
-      <ContactSection />
-      <Messaging />
+    <div className="bg-red-100 w-full h-[90vh] flex text-5xl text-black">
+      LOGIN PLZZZ
     </div>
   );
 };
