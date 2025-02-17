0ed8aa314136c8903826a395df19afa447f9a0c4
Aatmiyata Pokhrel <aatmiyatap@gmail.com>
2025-02-17 18:32:23 +0545

    order fetching changes

--- src/app/api/orders/[type]/route.ts
+++ src/app/api/orders/[type]/route.ts
@@ -1,12 +1,7 @@
-// app/api/orders/[type]/route.ts
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-
-//hardcoded test user IDs
-const TEST_SELLER_ID = "cm66ug29w0000v7ls10ac6rga";
-const TEST_BUYER_ID = "cm66tk8wx0001v7sob2kw46yi";
 
 export async function GET(
   request: Request,
