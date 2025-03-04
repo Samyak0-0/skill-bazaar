089a4c64bacbfc1168ebd046d9ba6c0c6c2255c5
weweaaawee <aakriti.pandeyyy@gmail.com>
2025-03-04 23:54:39 +0545

    routes to noti

--- src/components/notification_cmt/routes/notification.js
+++ src/components/notification_cmt/routes/notification.js
@@ -3,7 +3,6 @@
 import Notification from '../models/notification.js';
 import { getServerSession } from "next-auth";
 import { authOptions } from '../pages/api/auth/[...nextauth]';
-// import { getSession } from ''; 
 
 const router = express.Router();
 
